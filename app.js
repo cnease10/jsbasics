f81ee7a0ff14c0ba717f56564ae235083f4efe07
cnease10 <cierra.nease10@gmail.com>
2020-09-01 21:24:40 -0700

    fizzbuzz

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ for (let i = 0; i <= 200; i++) {
         console.log(i)
     }
 }
+
+for (let i = 0; i <20; i++) {
+    console.log('repeater')
+}
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 ==0 && i % 5 == 0) {
+        console.log("fizzbuzz")
+    }
+    else if (i % 3 == 0) {
+        console.log("fizz")
+    }
+    else if (i % 5 == 0) {
+        console.log("buzz")
+    }
+    else {
+        console.log(i)
+    }
+}
